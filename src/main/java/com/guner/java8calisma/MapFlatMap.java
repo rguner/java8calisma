01f4b2e79fc91896414bfc4053a852c0e64f24e3
Ramazan Güner <ramazan.guner@atmosware.turkcell.com.tr>
2026-04-01 11:19:24 +0300

    flat map example

--- src/main/java/com/guner/java8calisma/MapFlatMap.java
+++ src/main/java/com/guner/java8calisma/MapFlatMap.java
@@ -13,6 +13,7 @@ public class MapFlatMap {
         m.map1();
         m.flatMap1();
         m.flatMap2();
+        m.flatMap3();
     }
 
     private void flatMap2() {
@@ -51,6 +52,41 @@ public class MapFlatMap {
                 .stream()
                 .flatMap(Collection::stream)
                 .collect(Collectors.toList()));
+    }
+
+    private void flatMap3() {
+        // Creating a list of prime numbers
+        List<Integer> primeNumbers
+                = Arrays.asList(5, 7, 11, 13);
+
+        // Creating a list of odd numbers
+        List<Integer> oddNumbers = Arrays.asList(1, 3, 5);
+
+        // Creating a list of even numbers
+        List<Integer> evenNumbers
+                = Arrays.asList(2, 4, 6, 8);
+
+        // Combining the above lists into a list of lists
+        List<List<Integer> > listOfListofInts
+                = Arrays.asList(primeNumbers, oddNumbers,
+                evenNumbers);
+
+        // Printing the structure before flattening
+        System.out.println(
+                "The Structure before flattening is : "
+                        + listOfListofInts);
+
+        // Flattening the list of lists into a single list
+        // using flatMap
+        List<Integer> listofInts
+                = listOfListofInts.stream()
+                .flatMap(list -> list.stream())
+                .collect(Collectors.toList());
+
+        // Printing the structure after flattening
+        System.out.println(
+                "The Structure after flattening is : "
+                        + listofInts);
     }
 
 
